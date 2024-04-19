b7fcae02fc0a2fd9a5fa8776e18778b20c76b13c
kaiweifan11 <30461398+kaiweifan11@users.noreply.github.com>
2024-04-20 01:57:10 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # RazerProfile
-RazerProfile
+
+to start, run the following command:
+**$ npm start**
+
+Steps to run:
+
+Step 1: Go to directory and run 
+$ npm install
+
+Step 2: run
+$ npm start
+
+step 3: go to http://localhost:9000/ in your browser
+If you'd like to change the port, update the line in package.json
+"start": "webpack serve --mode development --port 9000",
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    hot: true,
+    open: true,
+  },
+};
